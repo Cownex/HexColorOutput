22b013acbb17e1bbef120a39fc4aa2c3ec5a5ffb
Cownex <justingumlich@gmail.com>
2022-07-08 19:32:05 +0200

    v1.1

--- setup.py
+++ setup.py
@@ -3,13 +3,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='hex_color_output',
-    version='1.0.3',
+    version='1.1',
     license='MIT',
     author="Cownex",
-    description="Hexcolor print",
+    description="Color the output with Hex Color Codes.",
     author_email='contact@cownex.de',
     packages=find_packages('source'),
     package_dir={'': 'source'},
+    
+    long_description_content_type="text/markdown",
     url='https://github.com/Cownex/HexOutput',
     keywords='python hex color terminal output color-output',
 )
